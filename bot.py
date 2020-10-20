30d045eff9b267aac58385f9e37ce1cf64e166b3
bhespiritu <lozman1@elne.org>
2020-10-19 22:24:43 -0400

    improve command support

--- bot.py
+++ bot.py
@@ -4,13 +4,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import commands
+
 client = discord.Client()
 
+command_dict = {
+    "cheeseburger": commands.cheeseburger
+}
+
 token = str(os.getenv("TOKEN"))
+prefix = 'AM'
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -19,5 +26,12 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        
+    if(message.content.startswith(prefix)):
+        command = message.content[len(prefix):] #strip the prefix
+        for opcode, method in command_dict:
+            if opcode == command:
+                await method(client, message)
+        
 
 client.run(token)
